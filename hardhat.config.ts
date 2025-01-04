00c6d82353e796dbd64321dca9df43ecc989f86e
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-01-04 10:29:40 +0100

    Update at Sat, 04 Jan 2025 09:29:40 GMT

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { type HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+	solidity: "0.8.28",
 };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+	const provider = hre.ethers.provider;
+
+	for (const account of accounts) {
+		console.log(
+			"%s (%i ETH)",
+			account.address,
+			hre.ethers.formatEther(
+				// getBalance returns wei amount, format to ETH amount
+				await provider.getBalance(account.address),
+			),
+		);
+	}
+});
 
 export default config;
