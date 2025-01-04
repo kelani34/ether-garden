1b39185338e96d53a6e5f7b2cd279a28d5ecd47a
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-01-04 01:25:58 +0100

    Update at Sat, 04 Jan 2025 00:25:58 GMT

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="">
-      <Image
-    </div>
+			<Image
+				src="/images/nextjs.png"
+				alt="Next.js Logo"
+				width={500}
+				height={500}
+			/>
+		</div>
 	);
 }
