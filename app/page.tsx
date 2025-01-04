dd37a5458b7571b7d9a3d6c181ceebcbcbaa3979
Shina-Kelani Taiwo <kelanitaiwo34@gmail.com>
2025-01-04 03:20:35 +0100

    Update at Sat, 04 Jan 2025 02:20:35 GMT

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,39 @@
 import Image from "next/image";
 
 export default function Home() {
+	async function main() {
+		// Create a random wallet
+		const wallet = "0xdD2FD4581271e230360230F9337D5c0430Bf44C0";
+	
 
-  const { ethers, network } = require("hardhat");
+		// Get the first signer (default account) from Hardhat
+		const [deployer] = await ethers.getSigners();
 
-		async function main() {
-			// Create a random wallet
-			const wallet = ethers.Wallet.createRandom();
-			console.log("New account address:", wallet.address);
-			console.log("Private key:", wallet.privateKey);
+		// Set the balance of the new account to 10 ETH (10 * 10^18 wei)
+		const balance = "0x8ac7230489e80000";
+		await network.provider.send("hardhat_setBalance", [
+			wallet.address,
+			balance,
+		]);
 
-			// Get the first signer (default account) from Hardhat
-			const [deployer] = await ethers.getSigners();
+		console.log("Funded new account with 10 ETH");
+	}
 
-			// Set the balance of the new account to 10 ETH (10 * 10^18 wei)
-			const balance = "0x8ac7230489e80000";
-			await network.provider.send("hardhat_setBalance", [
-				wallet.address,
-				balance,
-			]);
+	main()
+		.then(() => process.exit(0))
+		.catch((error) => {
+			console.error(error);
+			process.exit(1);
+		});
 
-			console.log("Funded new account with 10 ETH");
-		}
-
-		main()
-			.then(() => process.exit(0))
-			.catch((error) => {
-				console.error(error);
-				process.exit(1);
-			});
-
-
-      
-		return (
-			<div className="">
-				<Image
-					src="/images/nextjs.png"
-					alt="Next.js Logo"
-					width={500}
-					height={500}
-				/>
-			</div>
-		);
+	return (
+		<div className="">
+			<Image
+				src="/images/nextjs.png"
+				alt="Next.js Logo"
+				width={500}
+				height={500}
+			/>
+		</div>
+	);
 }
